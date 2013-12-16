688848dbb32ef0cf2bde2d44425d86f7117c34c1
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2013-12-16 13:15:57 -0200

    added pour watch

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "coffe"
 coffee -c ajaxpagination.coffee
+echo "uglifyjs"
 uglifyjs ajaxpagination.js -o ajaxpagination.min.js
+echo "gzip"
 gzip -k -f ajaxpagination.min.js
+echo ""
---
+++ pour_watch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true
+do
+    date
+    ./compile.sh
+    sleep 5
+done
